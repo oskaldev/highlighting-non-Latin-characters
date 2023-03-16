46b84d40503af736acbbc0595535378df5bb85e2
oskal314 <artem-osipovartem3002@mail.ru>
2023-03-16 20:41:37 +0300

    update

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 const innerText = document.querySelector('#latin-text');
 const outerText = document.querySelector('.latin-result>pre>code');
-const template = '0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz!@#$%^&*()_+-={}[]?.,;:\'\"\\/ ';
+
+
+// const template = '0123456789AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz!@#$%^&*()_+-={}[]?.,;:\'\"\\/ ';
+// document.querySelector('button').onclick = () => {
+//   let text = innerText.value.trim();
+//   if (text === '') return;
+//   let out = '';
+//   for (let i = 0; i < text.length; i++) {
+//     if (text[ i ] === '>') {
+//       out += '&#x3E';
+//     }
+//     else if (text[ i ] === '<') {
+//       out += '&#x3C';
+//     }
+//     else if (text[ i ] === '\n') {
+//       out += '<br>';
+//     }
+//     else if (template.indexOf(text[ i ]) === -1) {
+//       out += '<mark>' + text[ i ] + '</mark>';
+//     }
+//     else {
+//       out += text[ i ];
+//     }
+//   }
+//   console.log(out);
+//   outerText.innerHTML = out;
+// }
 
 document.querySelector('button').onclick = () => {
+
   let text = innerText.value.trim();
   if (text === '') return;
-  let out = '';
-  for (let i = 0; i < text.length; i++) {
-    if (text[ i ] === '\n') {
-      out += '<br>';
-    } else if (template.indexOf(text[ i ]) === -1) {
-      out += '<mark>' + text[ i ] + '</mark>';
-    } else {
-      out +=  text[ i ];
-    }
-  }
-  outerText.innerHTML = out;
+  let result;
+  const regexp = /[^\w\s'",\\.:;?!@#$%^&*+-={}\[\]()<>]/gi;
+
+  result = text.replace(/\>/g, str => '&#x3E');
+  result = result.replace(/\</g, str => '&#x3C');
+  result = result.replace(regexp, symbol => '<mark>' + symbol + '</mark>');
+
+
+  console.log(result);
+  outerText.innerHTML = result;
 }
+
+
